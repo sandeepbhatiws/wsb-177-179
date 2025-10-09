eacda8afde4c41915d2c15f6ed194a2a385e330c
Sandeep Bhati <sandeep@gmail.com>
2025-10-09 19:06:30 +0530

    Changes

---
+++ ecommerce/src/Components/Home.jsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react'
+import Header from './Common/Header'
+import Footer from './Common/Footer'
+import ProductCard from './Common/ProductCard'
+import axios from 'axios'
+import { toast } from 'react-toastify'
+
+export default function Home() {
+
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        axios.get('https://dummyjson.com/products')
+        .then((result) => {
+            setProducts(result.data.products);
+            console.log(result.data.products)
+        })
+        .catch(() => {
+            toast.error('Something went wrong !')
+        })
+    }, []);
+
+
+    return (
+        <>
+            <Header />
+
+            <div className='container-fluid'>
+                <div class="container bg-white">
+                    <nav class="navbar navbar-expand-md navbar-light bg-white">
+                        <div class="container-fluid m-3 justify-content-center text-center p-0"> <a class="navbar-brand  text-uppercase fw-800" href="#"><span class="border-red pe-2">New</span>Product</a> 
+                        </div>
+                    </nav>
+                    <div class="row">
+                        
+                        {
+                            products.map(() => {
+                                return(
+                                    <ProductCard/>
+                                )
+                            })
+                        }
+                        
+                    </div>
+                </div>
+            </div>
+
+
+            <Footer />
+        </>
+    )
+}
---
+++ ecommerce/src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import Home from './Components/Home'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import './assets/css/style.css'
+
+createRoot(document.getElementById('root')).render(
+  <>
+
+    <Home />
+  </>,
+)
